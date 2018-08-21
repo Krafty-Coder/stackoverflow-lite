621ba66f7dd965779326601529a4922438a1c3ab
Malcolm Peter <kraftycoder@gmail.com>
2018-08-21 06:24:00 +0100

    [Feature #160152699] Add api for homepage

--- app/api.py
+++ app/api.py
@@ -17,37 +17,6 @@ class Home(Resource):
         return jsonify({'message': 'Stackoverflow-lite, the alternate app if they get to hack Stackoverflow *.* '})
 
 
-class Login(Resource):
-    def post(self):
-        username = request.get_json()['username']
-        password_candidate = request.get_json()['password']
-
-        result = cur.execute(
-            "SELECT * FROM users WHERE username = %s",
-            [username])
-
-        if result > 0:
-            # Get stored hash
-            data = cur.fetchone()
-            password = data['password']
-
-            # Compare Passwords
-            cur.close()
-            if sha256_crypt.verify(password_candidate, password):
-                # Password and username matches
-                session['logged_in'] = True
-                session['username'] = username
-                success = 'Successfully logged in'
-                return {'message': success}
-
-            else:
-                error = 'Password or username incorrect, Invalid login'
-                return {'message': error}
-        else:
-            error = "Username not found"
-            return {'message': error}
-
-
 api.add_resource(Home, '/api/v1/', endpoint = 'homepage')
 if __name__ == '__main__':
     app.run(debug=True)
