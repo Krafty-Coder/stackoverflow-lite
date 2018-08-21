3fe0e6f7fc8a4fca350b9870402b96ba3f9db83c
Malcolm Peter <kraftycoder@gmail.com>
2018-08-21 16:17:00 +0100

    Restore models.py file in order to use branches

--- app/models.py
+++ app/models.py
@@ -2,27 +2,3 @@ import psycopg2
 
 conn = psycopg2.connect("dbname=stackoverflowlite user=postgres password=adminray host=localhost")  # Connecting to the database
 cur = conn.cursor()  # Activate connection using the cursor
-
-cur.execute('''CREATE TABLE IF NOT EXISTS questions(
-    id serial PRIMARY KEY,
-    title varchar (50) NOT NULL,
-    description varchar (100) NOT NULL,
-    timestamp timestamp default current_timestamp
-    ) ''')
-
-cur.execute('''CREATE TABLE IF NOT EXISTS answers(
-    id serial PRIMARY KEY,
-    description varchar (200) NOT NULL,
-    timestamp timestamp default current_timestamp
-    ) ''')
-
-cur.execute('''CREATE TABLE IF NOT EXISTS users(
-    id serial PRIMARY KEY,
-    username varchar (50) NOT NULL,
-    email varchar (100) NOT NULL,
-    password varchar (100) NOT NULL,
-    password_confirmation varchar (100) NOT NULL,
-    timestamp timestamp default current_timestamp
-    ) ''')
-
-conn.commit()
