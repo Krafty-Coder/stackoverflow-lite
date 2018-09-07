07580728b19495a18693906f868eddd95e37bdf2
Malcolm Peter <kraftycoder@gmail.com>
2018-09-07 09:58:52 +0300

    made some changes

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-conn = psycopg2.connect("dbname=stackoverflowlite user=postgres password=adminray host=localhost")  # Connecting to the database
+conn = psycopg2.connect("dbname=stackoverflowlite user=postgres password=adminray host=https://infinite-forest-80808.herokuapp.com/")  # Connecting to the database
 cur = conn.cursor()  # Activate connection using the cursor
 
 
@@ -16,5 +16,12 @@ cur.execute('''CREATE TABLE IF NOT EXISTS users(
 cur.execute('''CREATE TABLE IF NOT EXISTS answers(
     id serial PRIMARY KEY,
     description varchar (200) NOT NULL,
+    timestamp timestamp default current_timestamp
+    ) ''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS questions(
+    id serial PRIMARY KEY,
+    title varchar (50) NOT NULL,
+    description varchar (100) NOT NULL,
     timestamp timestamp default current_timestamp
     ) ''')
