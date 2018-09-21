a24bfd7ad922109f9c645ffc456223cff1cd7c28
Malcolm Peter <kraftycoder@gmail.com>
2018-09-21 17:39:18 +0300

    delete db

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ import psycopg2
 conn = psycopg2.connect("dbname=d35r4tfslcr7uf user=hantjxrfgrzzrr password=d6a32d0baa1e998ef650ff3ecc05a427eca2595631125cc23d19526779398203 host=ec2-54-221-237-246.compute-1.amazonaws.com port=5432")  # Connecting to the database
 cur = conn.cursor()  # Activate connection using the cursor
 
+cur.exectute('''DELETE * FROM DATABASE''')
 
 cur.execute('''CREATE TABLE users(
     id serial PRIMARY KEY,
