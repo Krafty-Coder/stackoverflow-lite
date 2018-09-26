57979db34bbe757e46215b75f3b1250644a9ec82
Malcolm Peter <kraftycoder@gmail.com>
2018-09-26 20:08:25 +0300

    set environment variables for the app

--- app/models.py
+++ app/models.py
@@ -1,6 +1,13 @@
+import os
+
 import psycopg2
 
-conn = psycopg2.connect("dbname=d35r4tfslcr7uf user=hantjxrfgrzzrr password=d6a32d0baa1e998ef650ff3ecc05a427eca2595631125cc23d19526779398203 host=ec2-54-221-237-246.compute-1.amazonaws.com port=5432")  # Connecting to the database
+dbasename = os.environ.get('DBASE_NAME')
+dbaseuser = os.environ.get('DBASE_USER')
+dbasepass = os.environ.get('DBASE_PASS')
+dbasehost = os.environ.get('DBASE_HOST')
+
+conn = psycopg2.connect("dbname={} user={} password={} host={} port={}".format(dbasename, dbaseuser, dbasepass, dbasehost, ))  # Connecting to the database
 cur = conn.cursor()  # Activate connection using the cursor
 
 
