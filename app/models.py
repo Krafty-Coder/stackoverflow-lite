ad3cbd7cd2b9a1b3ce3b0426fa3b19c073cf33f0
Malcolm Peter <kraftycoder@gmail.com>
2018-09-27 10:49:21 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ dbaseuser = os.environ.get('DBASE_USER')
 dbasepass = os.environ.get('DBASE_PASS')
 dbasehost = os.environ.get('DBASE_HOST')
 
-conn = psycopg2.connect("dbname={} user={} password={} host={} port={}".format(dbasename, dbaseuser, dbasepass, dbasehost, ))  # Connecting to the database
+conn = psycopg2.connect("dbname={} user={} password={} host={} port={}".format(dbasename, dbaseuser, dbasepass, dbasehost))  # Connecting to the database
 cur = conn.cursor()  # Activate connection using the cursor
 
 
